223e531e29e7c30da2e5eabd6d1e8cb01f4c10a1
Varvara Efremova <varvara@an.isotropic.me>
2014-09-02 00:26:01 +1000

    Initial commit

---
+++ packet.py
@@ -0,0 +1,10 @@
+__author__ = "varvara"
+
+class Packet:
+    def __init__(self, address, channel, time, data):
+        self.address = address
+        self.channel = channel
+        self.time = time
+        self.data = data
+
+
