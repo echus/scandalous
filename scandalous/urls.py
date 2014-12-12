36f7aefc394836690daaa7ddf3c43575db03fe24
Anmol Thind <a.thind94@gmail.com>
2014-12-12 13:01:05 +1100

    Fixed packets folder not pushing

--- scandalous/urls.py
+++ scandalous/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'scandalous.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^packets/', include('packets.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('packets.urls')),
 )
