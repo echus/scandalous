5979a33d25cccf2dfff0d899460265b3f91b1fd7
Anmol Thind <a.thind94@gmail.com>
2015-02-20 14:34:52 +1100

    Added node and channel request functionality. Added admin forms for manual DB editing. Need to manually enter channels using admin forms.

--- scandalous/settings.py
+++ scandalous/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'packets',
+    'nodes',
 )
 
 MIDDLEWARE_CLASSES = (
--- scandalous/urls.py
+++ scandalous/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'scandalous.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^packets/', include('packets.urls')),
+    url(r'^nodes/', include('nodes.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
